1a664f9b971aef61d327447d8d6de85f19e0dfe4
Clifford Wolf <clifford@clifford.at>
2015-06-26 10:46:51 +0200

    Changed chip package in vivado examples

--- scripts/vivado/synth_area.tcl
+++ scripts/vivado/synth_area.tcl
@@ -2,7 +2,7 @@
 read_verilog ../../picorv32.v
 read_xdc synth_area.xdc
 
-synth_design -part xc7a15t-csg324 -top picorv32_axi
+synth_design -part xc7a15t-fgg484 -top picorv32_axi
 opt_design
 place_design
 route_design
