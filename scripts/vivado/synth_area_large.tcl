e72abc0284bb6a04b9a6f1ea8e5cab601181cbb4
Clifford Wolf <clifford@clifford.at>
2015-07-01 21:51:15 +0200

    Added vivado synth_area_{small,regular,large}.tcl scripts

--- scripts/vivado/synth_area_large.tcl
+++ scripts/vivado/synth_area_large.tcl
@@ -1,9 +1,8 @@
 
 read_verilog ../../picorv32.v
-read_verilog synth_area_top.v
 read_xdc synth_area.xdc
 
-synth_design -part xc7k70t-fbg676 -top top_large
+synth_design -part xc7k70t-fbg676 -top picorv32_axi
 opt_design -resynth_seq_area
 
 report_utilization
