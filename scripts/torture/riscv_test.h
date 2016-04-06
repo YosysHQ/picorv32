548abd6cce79c987cb051dd9e73caebafc3bcf40
Clifford Wolf <clifford@clifford.at>
2016-04-06 16:38:57 +0200

    Added scripts/torture for riscv-torture tests

---
+++ scripts/torture/riscv_test.h
@@ -0,0 +1,13 @@
+#ifndef RISCV_TEST_H
+#define RISCV_TEST_H
+
+#define RVTEST_RV32U
+#define RVTEST_CODE_BEGIN
+#define RVTEST_CODE_END
+#define RVTEST_DATA_BEGIN
+#define RVTEST_DATA_END
+
+#define RVTEST_FAIL sbreak
+#define RVTEST_PASS sbreak
+
+#endif
