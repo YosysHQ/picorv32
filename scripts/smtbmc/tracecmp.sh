8710809978644f66e4db963afb5e384631d8ae96
Clifford Wolf <clifford@clifford.at>
2016-08-30 12:44:03 +0200

    Using new yosys write_smt2 cmdline

--- scripts/smtbmc/tracecmp.sh
+++ scripts/smtbmc/tracecmp.sh
@@ -6,7 +6,7 @@ yosys -ql tracecmp.yslog \
         -p 'read_verilog -formal -norestrict -assume-asserts ../../picorv32.v' \
         -p 'read_verilog -formal tracecmp.v' \
 	-p 'prep -top testbench -nordff' \
-	-p 'write_smt2 -mem -bv -wires tracecmp.smt2'
+	-p 'write_smt2 -wires tracecmp.smt2'
 
 yosys-smtbmc -s yices --smtc tracecmp.smtc --dump-vcd output.vcd --dump-smtc output.smtc tracecmp.smt2
 
