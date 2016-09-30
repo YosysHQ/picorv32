a6f5bc4f0524f3a0924e0840eeb2ea5f2f26419c
Clifford Wolf <clifford@clifford.at>
2016-09-30 18:18:32 +0200

    Added smtbmc axicheck

---
+++ scripts/smtbmc/axicheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+yosys -ql axicheck.yslog \
+	-p 'read_verilog -formal -norestrict -assume-asserts ../../picorv32.v' \
+	-p 'read_verilog -formal axicheck.v' \
+	-p 'prep -top testbench -nordff' \
+	-p 'write_smt2 -wires axicheck.smt2'
+
+yosys-smtbmc -s boolector --dump-vcd output.vcd --dump-smtc output.smtc axicheck.smt2
+
