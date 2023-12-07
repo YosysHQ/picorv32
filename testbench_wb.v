50877f90512d8285b779d223c2ce3039533140a3
eebear <43470847+rede97@users.noreply.github.com>
2023-12-07 20:47:50 +0800

    Update testbench_wb.v

--- testbench_wb.v
+++ testbench_wb.v
@@ -20,7 +20,7 @@ module testbench #(
 			$dumpfile("testbench.vcd");
 			$dumpvars(0, testbench);
 		end
-		repeat (1000000) @(posedge clk);
+		repeat (100000) @(posedge clk);
 		$display("TIMEOUT");
 		$finish;
 	end
@@ -83,8 +83,89 @@ module picorv32_wrapper #(
 	wire wb_m2s_we;
 	wire wb_m2s_cyc;
 	wire wb_m2s_stb;
-	wire [31:0] wb_s2m_dat;
-	wire wb_s2m_ack;
+	reg [31:0] wb_s2m_dat;
+	reg wb_s2m_ack;
+
+	localparam INNER_ADDR_BASE = 32'h0000_0000;
+	localparam INNER_ADDR_WIDTH = 30;
+	localparam BRIDGE_ADDR_BASE = 32'h4000_0000;
+	localparam BRIDGE_ADDR_WIDTH = 12;
+
+	reg         bridge_wb_stb;
+	wire [31:0] bridge_wb_dat_o;
+	wire        bridge_wb_ack_o;
+
+
+	reg         ram_wb_stb;
+	wire [31:0] ram_wb_dat_o;
+	wire        ram_wb_act_o;
+
+	always @(*) begin
+		if (((wb_m2s_adr ^ INNER_ADDR_BASE) >> INNER_ADDR_WIDTH) == 32'd0) begin
+			bridge_wb_stb = 1'b0;
+			ram_wb_stb    = wb_m2s_stb;
+			wb_s2m_dat    = ram_wb_dat_o;
+			wb_s2m_ack    = ram_wb_act_o;
+		end else if (((wb_m2s_adr ^ BRIDGE_ADDR_BASE) >> BRIDGE_ADDR_WIDTH) == 32'd0) begin
+			ram_wb_stb    = 1'b0;
+			bridge_wb_stb = wb_m2s_stb;
+			wb_s2m_dat    = bridge_wb_dat_o;
+			wb_s2m_ack    = bridge_wb_ack_o;
+		end
+	end
+
+	wire                    pclk;
+	wire                    prestn;
+	// APB interface 
+	wire [BRIDGE_ADDR_WIDTH-1:0]   paddr;
+	wire                    psel;
+	wire                    penable;
+	wire                    pwrite;
+	wire [31:0]             pwdata;
+	wire                    pready;
+	wire [31:0]             prdata;
+	wire                    pslverr;
+
+	wishbone2apb #(
+		.ADDR_WIDTH(BRIDGE_ADDR_WIDTH)
+	) bridge (
+		.wb_clk_i(wb_clk),
+		.wb_rst_i(wb_rst),
+
+		.wb_adr_i(wb_m2s_adr),
+		.wb_dat_i(wb_m2s_dat),
+		.wb_stb_i(bridge_wb_stb),
+		.wb_cyc_i(wb_m2s_cyc),
+		.wb_dat_o(bridge_wb_dat_o),
+		.wb_ack_o(bridge_wb_ack_o),
+		.wb_sel_i(wb_m2s_sel),
+		.wb_we_i(wb_m2s_we),
+		.pclk(pclk),
+		.prestn(prestn),
+		// APB interface 
+		.paddr(paddr),
+		.psel(psel),
+		.penable(penable),
+		.pwrite(pwrite),
+		.pwdata(pwdata),
+		.pready(pready),
+		.prdata(prdata),
+		.pslverr(pslverr)
+	);
+
+	regfile regcfg(
+		.PCLK(pclk),
+		.PRESETn(prestn),
+		.PSEL(psel),
+		.PENABLE(penable),
+		.PADDR(paddr),
+		.PWRITE(pwrite),
+		.PWDATA(pwdata),
+		.PRDATA(prdata),
+		.PREADY(pready),
+		.PSLVERR(pslverr)
+	);
+
 
 	wb_ram #(
 		.depth (128*1024),
@@ -95,10 +176,10 @@ module picorv32_wrapper #(
 
 		.wb_adr_i(wb_m2s_adr),
 		.wb_dat_i(wb_m2s_dat),
-		.wb_stb_i(wb_m2s_stb),
+		.wb_stb_i(ram_wb_stb),
 		.wb_cyc_i(wb_m2s_cyc),
-		.wb_dat_o(wb_s2m_dat),
-		.wb_ack_o(wb_s2m_ack),
+		.wb_dat_o(ram_wb_dat_o),
+		.wb_ack_o(ram_wb_act_o),
 		.wb_sel_i(wb_m2s_sel),
 		.wb_we_i(wb_m2s_we),
 
@@ -290,4 +371,156 @@ module wb_ram #(
 		if (memfile != "")
 			$readmemh(memfile, mem);
 	end
+endmodule
+
+
+module wishbone2apb #(
+    parameter ADDR_WIDTH = 10
+) (
+    // Wishbone clock
+    input  wire wb_clk_i,
+	input  wire wb_rst_i,
+    // Wishbone interface
+	input  wire [ADDR_WIDTH-1:0] wb_adr_i,
+	input  wire [31:0] wb_dat_i,
+	input  wire [3:0] wb_sel_i,
+	input  wire wb_we_i,
+	input  wire wb_cyc_i,
+	input  wire wb_stb_i,
+	output wire wb_ack_o,
+	output wire [31:0] wb_dat_o,
+
+    // APB clock
+    output wire                    pclk,
+    output wire                    prestn,
+    // APB interface 
+    output wire [ADDR_WIDTH-1:0]   paddr,
+    output wire                    psel,
+    output wire                    penable,
+    output wire                    pwrite,
+    output wire [31:0]             pwdata,
+    input wire                     pready,
+    input wire [31:0]              prdata,
+    input wire                     pslverr
+);
+
+
+reg                  wb_stb_d;
+reg [ 3:0]           wb_sel_d;
+reg [ADDR_WIDTH-1:0] wb_adr_d;
+reg [31:0]           wb_dat_d;
+reg                  wb_we_d;
+
+always @(posedge wb_clk_i or posedge wb_rst_i) begin
+    if (wb_rst_i) begin
+		wb_stb_d <= 1'b0;
+        wb_sel_d <= 4'b0;
+        wb_adr_d <= {32{1'd0}};
+        wb_dat_d <= 32'd0;
+        wb_we_d  <= 1'b0;
+    end else begin
+        if (wb_stb_i & (wb_stb_d ? wb_ack_o : 1'b1)) begin
+            wb_stb_d <= 1'b1;
+            wb_sel_d <= wb_sel_i;
+            wb_adr_d <= wb_adr_i;
+            wb_dat_d <= wb_dat_i;
+            wb_we_d  <= wb_we_i;
+        end else if (wb_ack_o | (!psel)) begin
+            wb_stb_d <= 1'b0;
+            wb_sel_d <= 4'b0;
+            wb_adr_d <= {32{1'd0}};
+            wb_dat_d <= 32'd0;
+            wb_we_d  <= 1'b0;
+        end
+    end
+end
+
+reg wb_ack_d;
+always @(posedge wb_clk_i or posedge wb_rst_i) begin
+    if (wb_rst_i) begin
+        wb_ack_d <= 1'b0;
+    end else begin
+        wb_ack_d <= wb_ack_o;
+    end
+end
+
+
+assign pclk = wb_clk_i;
+assign prestn = !wb_rst_i;
+assign psel = wb_cyc_i & (wb_stb_i | wb_stb_d);
+assign penable = wb_cyc_i & wb_stb_d & (!wb_ack_d);
+
+assign paddr = wb_stb_d ? wb_adr_d : wb_adr_i;
+assign pwdata = wb_stb_d ? wb_dat_d : wb_dat_i;
+assign pwrite = wb_stb_d ? wb_we_d : wb_we_i;
+
+assign wb_ack_o = penable & pready;
+assign wb_dat_o = prdata;
+
+always @(wb_clk_i) begin
+    if (pslverr) begin
+        $display("Wishbone to APB: slave error! addr: 0x%08x write: %d wdata: 0x%08x rdata: 0x%08x", paddr, pwrite, pwdata, prdata);
+        $finish;
+    end
+end
+    
+endmodule
+
+module regfile #(
+    parameter ADDR_WIDTH = 12
+)
+  (
+   input wire        PRESETn,
+   input wire        PCLK,
+   input wire        PSEL,
+   input wire        PENABLE,
+   input wire [ADDR_WIDTH-1:0] PADDR,
+   input wire        PWRITE,
+   input wire [31:0] PWDATA,
+   output reg [31:0] PRDATA,
+   output wire       PREADY,
+   output reg        PSLVERR,
+
+   output reg [31:0] config_reg0,
+   output reg [31:0] config_reg1
+   );
+   reg               rready;
+
+   //READ
+   always @(posedge PCLK)
+     case(PADDR[ADDR_WIDTH-1:2]) 
+        0: PRDATA <= config_reg0;
+        1: PRDATA <= config_reg1;
+        default: PRDATA <= 32'hDEADBEEF;
+     endcase // case (PADDR)
+
+
+   assign PREADY = PWRITE ? PENABLE : rready;
+   
+   always @(posedge PCLK or negedge PRESETn) begin
+      if(~PRESETn) begin
+         rready <= 0;
+      end
+      else begin
+         rready <= 0;
+         if(~PWRITE & PSEL & ~rready)
+           rready <= 1'b1;
+      end
+   end
+
+   always @(posedge PCLK or negedge PRESETn)
+     if(~PRESETn) begin
+        PSLVERR <= 1'b0;
+        config_reg0 <= 32'habcd1234;
+        config_reg1 <= 32'hdc72ef98;
+     end
+     else begin
+        PSLVERR <= 1'b0;
+        if(PSEL & PENABLE & PWRITE)
+          case(PADDR[ADDR_WIDTH-1:2]) 
+            0: config_reg0 <= PWDATA;
+            1: config_reg1 <= PWDATA;
+          endcase // case (PADDR)
+     end // else: !if(~PRESETn)
+ 
 endmodule
